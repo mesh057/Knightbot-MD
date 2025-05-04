f50731b7ce9384f16662f1c8eb09741cea6b4630
mesh057 <brightbanter003@gmail.com>
2025-05-04 07:27:42 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight',
   author: 'Bot',
   botName: "Knight Bot",
-  botOwner: '@Mesh057', // Your name
-  ownerNumber: '254746844168', //Your number
+  botOwner: '@Mesh357', // Your name
+  ownerNumber: '+254746844168', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
